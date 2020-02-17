ad0937d4736f1025269c27d9864b16cde261bc3e
kulloveth <kulloveth@gmail.com>
2020-02-17 12:55:15 +0100

    created a callback for inserting data once in room

--- app/src/main/java/kulloveth/developer/com/pagingsample/db/ResultDatabase.java
+++ app/src/main/java/kulloveth/developer/com/pagingsample/db/ResultDatabase.java
@@ -2,9 +2,13 @@ package kulloveth.developer.com.pagingsample.db;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import java.util.concurrent.Executors;
 
 import kulloveth.developer.com.pagingsample.model.Result;
 
@@ -18,8 +22,27 @@ public abstract class ResultDatabase extends RoomDatabase {
     public static synchronized ResultDatabase getDatabase(Context context) {
         if (INSTANCE == null) {
             INSTANCE = Room.databaseBuilder(context, ResultDatabase.class, "result-db")
+                    .addCallback(roomCallback)
                     .build();
         }
         return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            insertResult();
+        }
+    };
+
+    private static void insertResult() {
+        Executors.newSingleThreadExecutor().execute(() -> {
+            Result result = new Result("Loveth", "Female");
+            Result result2 = new Result("Blake", "Male");
+            ResultDao resultDao = INSTANCE.resultDao();
+            resultDao.insert(result);
+            resultDao.insert(result2);
+        });
     }
 }
