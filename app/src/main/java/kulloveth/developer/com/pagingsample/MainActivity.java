9acc8ea47d44e42648c80e76448ead761107315d
kulloveth <kulloveth@gmail.com>
2020-02-17 15:09:17 +0100

    inserted raw json file into roo and displayed on the screen

--- app/src/main/java/kulloveth/developer/com/pagingsample/MainActivity.java
+++ app/src/main/java/kulloveth/developer/com/pagingsample/MainActivity.java
@@ -6,25 +6,33 @@ import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
 import kulloveth.developer.com.pagingsample.model.Result;
 import kulloveth.developer.com.pagingsample.ui.MainActivityViewModel;
+import kulloveth.developer.com.pagingsample.ui.ResultAdapter;
 
 public class MainActivity extends AppCompatActivity {
     private MainActivityViewModel mainActivityViewModel;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ResultAdapter adapter = new ResultAdapter();
+        recyclerView = findViewById(R.id.result_rv);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
         mainActivityViewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
         mainActivityViewModel.getAllResult().observe(this, results -> {
             for (Result result : results) {
                 Log.d("fetch", "onChanged:" + result.toString());
             }
+            adapter.submitList(results);
         });
     }
 }
--- app/src/main/java/kulloveth/developer/com/pagingsample/db/ResultDatabase.java
+++ app/src/main/java/kulloveth/developer/com/pagingsample/db/ResultDatabase.java
@@ -8,20 +8,31 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.concurrent.Executors;
 
+import kulloveth.developer.com.pagingsample.R;
 import kulloveth.developer.com.pagingsample.model.Result;
 
 @Database(entities = {Result.class}, version = 1, exportSchema = false)
 public abstract class ResultDatabase extends RoomDatabase {
 
+    private static Context activity;
     private static ResultDatabase INSTANCE;
 
     public abstract ResultDao resultDao();
 
     public static synchronized ResultDatabase getDatabase(Context context) {
+        activity = context.getApplicationContext();
         if (INSTANCE == null) {
-            INSTANCE = Room.databaseBuilder(context, ResultDatabase.class, "result-db")
+            INSTANCE = Room.databaseBuilder(activity, ResultDatabase.class, "result-db")
                     .addCallback(roomCallback)
                     .build();
         }
@@ -37,12 +48,51 @@ public abstract class ResultDatabase extends RoomDatabase {
     };
 
     private static void insertResult() {
+
+
         Executors.newSingleThreadExecutor().execute(() -> {
             Result result = new Result("Loveth", "Female");
             Result result2 = new Result("Blake", "Male");
             ResultDao resultDao = INSTANCE.resultDao();
             resultDao.insert(result);
             resultDao.insert(result2);
+            fillWithJsonData(activity);
         });
+    }
+
+    private static void fillWithJsonData(Context context){
+        ResultDao resultDao = getDatabase(context).resultDao();
+        JSONArray results = loadJsonArray(context);
+        try{
+            for(int i = 0; i<results.length(); i++){
+                JSONObject result = results.getJSONObject(i);
+                String resultName = result.getString("name");
+                String resultGender = result.getString("gender");
+                resultDao.insert(new Result(resultName,resultGender));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static JSONArray loadJsonArray(Context context) {
+        StringBuilder builder = new StringBuilder();
+        InputStream in = context.getResources().openRawResource(R.raw.ricky);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+        String line;
+
+        try {
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+
+            JSONObject jsonObject = new JSONObject(builder.toString());
+            return jsonObject.getJSONArray("results");
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
