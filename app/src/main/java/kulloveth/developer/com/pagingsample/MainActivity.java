ef77d2503baf4400ef97e518250a9cec3004e06e
kulloveth <kulloveth@gmail.com>
2020-02-17 13:16:36 +0100

    setup activity to observe data from viewModel

--- app/src/main/java/kulloveth/developer/com/pagingsample/MainActivity.java
+++ app/src/main/java/kulloveth/developer/com/pagingsample/MainActivity.java
@@ -1,14 +1,30 @@
 package kulloveth.developer.com.pagingsample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import java.util.List;
+
+import kulloveth.developer.com.pagingsample.model.Result;
+import kulloveth.developer.com.pagingsample.ui.MainActivityViewModel;
 
 public class MainActivity extends AppCompatActivity {
+    private MainActivityViewModel mainActivityViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainActivityViewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+        mainActivityViewModel.getAllResult().observe(this, results -> {
+            for (Result result : results) {
+                Log.d("fetch", "onChanged:" + result);
+            }
+        });
     }
 }
