fab21131a47e19708ab9cc90a8cd2da60fc51cda
kulloveth <kulloveth@gmail.com>
2020-02-17 13:00:57 +0100

    setup viewModel class to send data to the UI

--- app/src/main/java/kulloveth/developer/com/pagingsample/ui/MainActivityViewModel.java
+++ app/src/main/java/kulloveth/developer/com/pagingsample/ui/MainActivityViewModel.java
@@ -4,9 +4,32 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import kulloveth.developer.com.pagingsample.db.ResultRepository;
+import kulloveth.developer.com.pagingsample.model.Result;
 
 public class MainActivityViewModel extends AndroidViewModel {
+    private ResultRepository resultRepository;
+    private LiveData<List<Result>> allResult;
+
     public MainActivityViewModel(@NonNull Application application) {
         super(application);
+        resultRepository = new ResultRepository(application);
+        allResult = resultRepository.getAllResult();
+    }
+
+    public void insertResult(Result result){
+        resultRepository.insertResult(result);
+    }
+
+    public void deleteResult(Result result){
+        resultRepository.deleteResult(result);
+    }
+
+    public LiveData<List<Result>> getAllResult(){
+      return allResult;
     }
 }
